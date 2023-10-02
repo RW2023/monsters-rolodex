03f4dcf1731e24e5a0ce8ff9f345170793fc38b5
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-02 06:30:06 -0400

    cleanup to start

--- src/App.js
+++ src/App.js
@@ -2,54 +2,10 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: ""
-    };
-    console.log('constructor');
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount');
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        }
-        ));
-  }
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLowerCase();
-
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField);
-    });
-
     return (
-      <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={this.onSearchChange}
-        />
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+      <div className='App'>
+       <h1>It's Alive!</h1>
       </div>
     );
   }
