b7f73920ced2ef1a0a2ffff2a9cc39eaf30f234d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-02 07:19:35 -0400

    map lesson in this section we learn more about map and the importance of keys

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          id: "abcde",
+          name: 'Linda'
+        },
+        {
+          id: "fghij",
+          name: 'frank'
+        },
+        {
+          id: "klmno",
+          name: 'jacky'
+        },
+        {
+          id: "pqrst",
+          name: 'Andrei'
+        },
+      ]
+    };
+  }
   render() {
     return (
       <div className='App'>
-       <h1>It's Alive!</h1>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
 }
-
 export default App;
